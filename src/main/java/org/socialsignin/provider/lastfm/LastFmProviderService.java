94cebea409be0ddd8909521ccc80d533f8272085
Michael Lavelle <michael@lavelle.name>
2012-01-01 02:58:15 +0000

    Adding LastFmProviderService which can be easily autowired to give access to LastFm authenticated and unauthenticated apis

---
+++ src/main/java/org/socialsignin/provider/lastfm/LastFmProviderService.java
@@ -0,0 +1,26 @@
+package org.socialsignin.provider.lastfm;
+
+import org.socialsignin.provider.AbstractProviderService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.social.lastfm.api.LastFm;
+import org.springframework.social.lastfm.api.impl.LastFmTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LastFmProviderService extends AbstractProviderService<LastFm> {
+
+    @Value("${lastfm.consumerKey}")
+    private String lastFmApiKey;
+	
+	
+	@Override
+	public Class<LastFm> getApiClass() {
+		return LastFm.class;
+	}
+
+	@Override
+	public LastFm getUnauthenticatedApi() {
+		return new LastFmTemplate("spring-social-lastfm/1.0.1-SNAPSHOT",lastFmApiKey);
+	}
+
+}
